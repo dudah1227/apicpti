b4bf4eda422c040d83c537e1471301df8c86de64
dudah1227 <mefrancisco@faeterj-petropolis.edu.br>
2025-09-04 10:07:02 -0300

    Delete post directory

--- post/src/main/java/br/com/empresa/repository/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.empresa.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.empresa.entity.Post;
-
-public interface PostRepository extends JpaRepository<Post,Long>{
-
-}
